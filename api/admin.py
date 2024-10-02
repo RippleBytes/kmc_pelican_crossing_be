d2b6e5b4bc7068253eaf26625d102c831b1ffa15
Subrat Gyawali <subrat.gyawali@gmail.com>
2024-10-02 11:15:01 +0545

    docker updated

--- api/admin.py
+++ api/admin.py
@@ -8,11 +8,32 @@ from import_export.admin import ExportActionModelAdmin
 # admin.site.register(School)
 
 
+class EntranceExitInline(admin.StackedInline):
+    model = EntranceExit
+    extra = 1
+
+
+class RoadwayFacilityNearInline(admin.StackedInline):
+    model = RoadwayFacilityNear
+    extra = 1
+
+
+class RoadwayFarInline(admin.StackedInline):
+    model = RoadwayFacilityFar
+    extra = 1
+
+
+class FinalizationFormInline(admin.StackedInline):
+    model = FinalizationForm
+    extra = 1
+
+
 @admin.register(School)
 class SchoolAdmin(ExportActionModelAdmin, admin.ModelAdmin):
     list_display = ('school_name','school_type','ward')
     search_fields = ('school_name','school_type')
     list_filter = ('school_name',)
+    inlines = [EntranceExitInline, RoadwayFacilityNearInline, RoadwayFarInline, FinalizationFormInline]
 
 
 @admin.register(EntranceExit)
--- kmc_peligan_crossing/settings.py
+++ kmc_peligan_crossing/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0fw-pljwd@_&!h^@dtq2q@qjiz#a%8ry#l-42)$=@s1tk6s+%d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'quietly-relevant-raven.ngrok-free.app']
 
@@ -120,7 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
